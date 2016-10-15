ae170223472d96c5bfb4ada7622d2d68792d9d02
Hiren Joshi <hirenj@gmail.com>
2016-10-16 00:05:03 +0200

    Allow reading in previously generated data frames

--- test.js
+++ test.js
@@ -12,17 +12,37 @@ const Writer = require('./lib/object_writer');
 
 const dataframe = { "x" : [2,4,8,16,32], "y" : ["ab","ac","ad","ae","af"]};
 
+let obj_writer = null;
+
+// let framewriter = fs.createWriteStream('frame.Rda');
+
+// let obj_writer = new Writer(framewriter);
+
+// obj_writer.dataFrame(dataframe,["x","y"],["real","string"]);
+setTimeout(function() {
+
+// obj_writer.stream.end();
 
 let writer = fs.createWriteStream('test.Rda');
 
-let obj_writer = new Writer(writer);
+let written_frame = fs.createReadStream('frame.Rda');
+let written_frame2 = fs.createReadStream('frame.Rda');
+
+obj_writer = new Writer(writer);
 
 obj_writer.writeHeader();
-obj_writer.listPairs( {"frame" : dataframe},
-                      ["frame"],
-                      [ { "type" : "dataframe",
-                          "keys" : ["x","y"],
-                          "types" : ["real","string"]
-                        }
+obj_writer.listPairs( {"frame" : written_frame, "frame2" : written_frame2 },
+                      ["frame", "frame2"],
+                      [ 
                       ]);
+setTimeout(function() {
 obj_writer.stream.end();
+},2000);
+
+},1000);
+
+
+// { "type" : "dataframe",
+//                           "keys" : ["x","y"],
+//                           "types" : ["real","string"]
+//                         }
