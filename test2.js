d432beb5d413cbc65088a5fec8917edd1f9558ee
Hiren Joshi <hirenj@gmail.com>
2016-10-21 15:15:48 +0200

    Test writing super huge data frames

--- test2.js
+++ test2.js
@@ -17,6 +17,12 @@ let intwriter2 = fs.createWriteStream('ints2.Rda');
 const Readable = require('stream').Readable;
 const util = require('util');
 
+const stream_done_promise = function(stream) {
+  return new Promise(function(resolve) {
+    stream.on('close',resolve);
+  });
+};
+
 function IntStream(max,increment,options) {
   if (! (this instanceof IntStream)) return new IntStream(max,options);
   if (! options) options = {};
@@ -35,7 +41,6 @@ IntStream.prototype._read = function read() {
     self.push(self.count);
     self.count += self.increment;
   } else {
-    console.log("Ended");
     self.push(null);
     self.emit('close');
   }
@@ -44,18 +49,23 @@ IntStream.prototype._read = function read() {
 obj_writer = new Writer(intwriter1);
 obj_writer2 = new Writer(intwriter2);
 
-let intstream = new IntStream(100);
-let intstream2 = new IntStream(2*100,2);
+const vec_length = 1e05;
 
-let promise_1  = obj_writer.writeValue(intstream,"int").then(function() {
+let intstream = new IntStream(vec_length);
+let intstream2 = new IntStream(2*vec_length,2);
+
+let promise_1  = obj_writer.writeValue(intstream,"real").then(function() {
   console.log("Done writing int vector");
   obj_writer.stream.end();
-});
+}).then( () => stream_done_promise(intwriter1));
 
-let promise_2 = obj_writer2.writeValue(intstream2,"int").then(function() {
+let promise_2 = obj_writer2.writeValue(intstream2,"real").then(function() {
   console.log("Done writing second int vector");
   obj_writer2.stream.end();
-});
+}).then( () => stream_done_promise(intwriter2));
+
+intwriter1.on('close', () => console.log("Ended first stream"));
+intwriter2.on('close', () => console.log("Ended second stream"));
 
 Promise.all([promise_1,promise_2]).then(function() {
   console.log("Putting streams together");
@@ -73,7 +83,6 @@ Promise.all([promise_1,promise_2]).then(function() {
   obj_writer.writeHeader();
   obj_writer.listPairs( {"frame" : {"cola" : written_vector, "colb" : written_vector2 }},
                         ["frame"],
-                        [{ "type": "dataframe", "length" : 100, "keys": ["cola", "colb"], "types" : ["int","int"] }]
+                        [{ "type": "dataframe", "length" : vec_length, "keys": ["cola", "colb"], "types" : ["int","real"] }]
                         ).then( () => obj_writer.stream.end() );
-
 });
